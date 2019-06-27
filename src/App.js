105c7edca60acbe1ab090e9bd72104c36c9f6125
Dora Gil <git@doragil.me>
2019-06-27 19:58:03 -0400

    Created Header Component with CSS
    Co-authored-by: Nathan <nszelag@live.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header'
 
 class App extends Component {
   constructor(props){
@@ -75,11 +76,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing</h1>
-          <h3>Laptops</h3>
-          <h5>Customize your laptop</h5>  
-        </header>      
+        <Header />     
         <main>
           <section className="main__form">
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
