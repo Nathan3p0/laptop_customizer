642346ba09db7591e75d193dbe0edfadd959be83
Dora Gil <git@doragil.me>
2019-06-27 20:29:50 -0400

    Added form component. Not final. W/ Nate

---
+++ src/Components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+    return (  
+    <section className="main__form">
+        <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+        { features }
+    </section>
+    );
+}
+ 
+export default Form;
+
