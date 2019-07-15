22b98b5278aa766f999d094c7185f3b2b88a81cb
Nathan Szelag <nszelag@live.com>
2019-07-14 21:12:55 -0400

    Added summary total component

--- src/Components/MainSummary.js
+++ src/Components/MainSummary.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import Summary from './Summary';
+import SummaryTotal from './SummaryTotal';
 
 const MainSummary = (props) => {
 
@@ -11,13 +12,7 @@ const MainSummary = (props) => {
         <section className="main__summary">
             <h3>NEW GREENLEAF 2018</h3>
             {summary}
-            <div className="summary__total">
-                <div className="summary__total__label">Your Price: </div>
-                <div className="summary__total__value">
-                    {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
-                        .format(props.total)}
-                </div>
-            </div>
+            <SummaryTotal total={props.total} />
         </section>
     );
 }
